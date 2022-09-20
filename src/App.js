2d4a49862bbd7300fcfdad6f33bc4a3235509ab3
Hamafac <borysenko777@gmail.com>
2022-09-20 23:08:50 +0300

    install redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const dispatch = useDispatch();
+   const cash = useSelector(state => state.cash);
+   console.log(cash)
+
+   return (
+      <div className="App">
+         <div style={{ display: 'flex' }}>
+            <button>Пополнить счет</button>
+            <button>Снять со счета</button>
+         </div>
+      </div>
+   );
 }
 
 export default App;
